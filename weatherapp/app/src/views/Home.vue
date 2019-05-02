284118c0fda98a56ddcc61338ac3a15bc4e8b41b
ro33ah <ro33ah>
2019-05-02 12:42:28 +0200

    [ADD] combining frontend with backend

---
+++ weatherapp/app/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <WeatherInfo/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import WeatherInfo from "@/components/WeatherInfo.vue";
+import NotFound from "@/views/NotFound.vue";
+
+export default {
+  name: "home",
+  components: {
+    WeatherInfo, NotFound
+  }
+};
+</script>
